e92e9fab03e7a4804cce805ac4434bc1732d52ce
sixbeeshades <abhijith.p.subash@gmail.com>
2022-10-09 22:46:48 +0530

    nox-starting

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server😊😊😊");
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
