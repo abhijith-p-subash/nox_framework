ccf967b5396989d2b45a4c8f5568e25cbd656083
sixbeeshades <abhijith.p.subash@gmail.com>
2023-02-03 23:06:13 +0530

    helmet packege added

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import * as dotenv from "dotenv";
 import morgan from "morgan";
 import swaggerUi from "swagger-ui-express";
 import cors from "cors";
+import helmet from "helmet";
 
 const swaggerFile = require("../swagger_output.json");
 
@@ -23,6 +24,7 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
