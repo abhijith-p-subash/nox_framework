3d8c9b727598cde803a2d326cfd4a8dcb1b4cf3b
sixbeeshades <abhijith.p.subash@gmail.com>
2023-02-08 00:48:46 +0530

    rote index modification

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,10 +1,12 @@
-import { Router } from "express";
-import passport from "passport";
 import loginLogRouter from "./route/login-log.route";
 import userRouter from "./route/user.route";
 import authRouter from "./route/auth.route";
+import { Router } from "express";
+import passport from "passport";
 
 const routes = Router();
+
+export default routes;
 
 routes.use("/auth", authRouter);
 routes.use(
@@ -14,4 +16,4 @@ routes.use(
 );
 routes.use("/login-logs", loginLogRouter);
 
-export default routes;
+
